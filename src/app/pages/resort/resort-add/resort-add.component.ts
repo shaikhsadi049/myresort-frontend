34cfc30823e359ba29b36f40c4546e6f27f01002
Shaikh Sadi <shaikhsadi049@gmail.com>
2021-11-17 21:55:15 +0600

    ok

--- src/app/pages/resort/resort-add/resort-add.component.ts
+++ src/app/pages/resort/resort-add/resort-add.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-resort-add',
   templateUrl: './resort-add.component.html',
-  styleUrls: ['./resort-add.component.scss']
+  styleUrls: ['./resort-add.component.scss'],
 })
 export class ResortAddComponent implements OnInit {
+  formId = 'addRessort';
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      resortName: ['', Validators.required],
+    });
   }
 
+  get resortName() {
+    return this.form.get('resortName');
+  }
+
+  onAddResort(value: any) {
+    console.log(value, `form-values`);
+  }
 }
