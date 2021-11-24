318d6a66f5245a392079621d1e11b86c39a1285d
Shaikh Sadi <shaikhsadi049@gmail.com>
2021-11-24 22:09:30 +0600

    add resort form

--- src/app/pages/resort/resort-add/resort-add.component.ts
+++ src/app/pages/resort/resort-add/resort-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-resort-add',
@@ -9,20 +10,69 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ResortAddComponent implements OnInit {
   formId = 'addRessort';
   form: FormGroup;
+  addResortSub: Subscription;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
       resortName: ['', Validators.required],
+      address: ['', Validators.required],
+      cellNo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(11),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      isWifiExist: [false, Validators.required],
+      isBreakfastExist: [false, Validators.required],
+      isParkingExist: [false, Validators.required],
+      isOutdoorPoolExist: [false, Validators.required],
+      isLaundryExist: [false, Validators.required],
+      isBBQExist: [false, Validators.required],
     });
   }
 
   get resortName() {
     return this.form.get('resortName');
   }
+  get address() {
+    return this.form.get('address');
+  }
+  get cellNo() {
+    return this.form.get('cellNo');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get isWifiExist() {
+    return this.form.get('isWifiExist');
+  }
+  get isBreakfastExist() {
+    return this.form.get('isBreakfastExist');
+  }
+  get isParkingExist() {
+    return this.form.get('isParkingExist');
+  }
+  get isOutdoorPoolExist() {
+    return this.form.get('isOutdoorPoolExist');
+  }
+  get isLaundryExist() {
+    return this.form.get('isLaundryExist');
+  }
+  get isBBQExist() {
+    return this.form.get('isBBQExist');
+  }
 
-  onAddResort(value: any) {
-    console.log(value, `form-values`);
+  onAddResort() {
+    if (this.form.valid) {
+      console.log(this.form.value, `form-values`);
+      // this.addResortSub =
+    } else {
+      alert(`ALL FIELD REQUIRED!!`);
+    }
   }
 }
