118dd2a7871cd080697629a7deb7cff774e3518e
Sadi <sadi@erainfotechbd.com>
2021-11-28 17:22:29 +0600

    List item incomplete

--- src/app/pages/resort/service/resort.service.ts
+++ src/app/pages/resort/service/resort.service.ts
@@ -3,29 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
-
-
-
 const baseUrl = 'http://localhost:3000/api/resort/add';
+const getUrl = 'http://localhost:3000/api/resort/list';
 
 @Injectable({
   providedIn: 'root',
 })
-
-
 export class ResortService {
-
-
   constructor(private http: HttpClient) {}
-
 
   resortAdd(obj: any): Observable<any> {
     return this.http.post(baseUrl, obj);
   }
 
-  
   resortList(): Observable<any> {
-    return this.http.get(baseUrl);
+    return this.http.get(getUrl);
   }
-
 }
